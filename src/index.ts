1b465600c030adf1be34053b27b5ef624fc6eb62
Elias <elias.burgstaller@gmx.at>
2022-05-19 18:05:16 +0200

    uncomment index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { Logger, LoggerConfig } from "./logger";
 
 export {Logger, LoggerConfig}
 
-const logger = new Logger({
-    output: './logs/dev.log'
-});
-logger.info('I am an info message');
-logger.warning('I am a warning message');
-logger.error('I am an error message');
+// const logger = new Logger({
+//     output: 'dev.log'
+// });
+// logger.info('I am an info message');
+// logger.warning('I am a warning message');
+// logger.error('I am an error message');
